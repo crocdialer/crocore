c2999d334bbca5b1fa6d48f87f2803ce6c7a566b
crocdialer <crocdialer@gmail.com>
2019-03-08 18:33:56 +0100

    hello crocore

---
+++ include/crocore/crocore.hpp
@@ -0,0 +1,46 @@
+// __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
+//
+// Copyright (C) 2012-2016, Fabian Schmidt <crocdialer@googlemail.com>
+//
+// It is distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt
+// __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
+
+#pragma once
+
+#ifdef WIN32
+#define CROC_API __declspec(dllexport)
+#else
+#define CROC_API
+#endif
+
+#include <cstring>
+#include <string>
+#include <vector>
+#include <memory>
+#include <functional>
+#include <algorithm>
+
+//! forward declare a class and define shared-, const-, weak- and unique-pointers for it.
+#define DEFINE_CLASS_PTR(CLASS_NAME)\
+class CLASS_NAME;\
+using CLASS_NAME##Ptr = std::shared_ptr<CLASS_NAME>;\
+using CLASS_NAME##ConstPtr = std::shared_ptr<const CLASS_NAME>;\
+using CLASS_NAME##WeakPtr = std::weak_ptr<CLASS_NAME>;\
+using CLASS_NAME##UPtr = std::unique_ptr<CLASS_NAME>;
+
+#if defined(__APPLE__)
+// forward declare boost io_service
+namespace boost{ namespace asio{ class io_context; } }
+namespace crocdialer{ using io_service_t = boost::asio::io_context; }
+#else
+// forward declare boost io_service
+namespace boost { namespace asio { class io_service; }}
+namespace crocore { using io_service_t = boost::asio::io_service; }
+#endif
+
+namespace cc = crocore;
+
+#include "Logger.hpp"
+#include "Utils.hpp"
---
+++ include/crocore/file_functions.hpp
@@ -0,0 +1,104 @@
+// __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
+//
+// Copyright (C) 2012-2016, Fabian Schmidt <crocdialer@googlemail.com>
+//
+// It is distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt
+// __ ___ ____ _____ ______ _______ ________ _______ ______ _____ ____ ___ __
+
+#pragma once
+
+#include "crocore/crocore.hpp"
+
+namespace crocore {
+namespace fs {
+
+using path = std::string;
+
+enum class FileType
+{
+    IMAGE, MODEL, AUDIO, MOVIE, DIRECTORY, FONT, OTHER, NOT_A_FILE
+};
+
+size_t get_file_size(const path &the_file_name);
+
+// manage known file locations
+const std::set<path> &get_search_paths();
+
+void add_search_path(const path &thePath, int the_recursion_depth = 0);
+
+void clear_search_paths();
+
+std::vector<std::string> get_directory_entries(const path &thePath,
+                                               const std::string &theExtension = "",
+                                               int the_recursion_depth = 0);
+
+std::vector<std::string> get_directory_entries(const path &thePath, FileType the_type,
+                                               int the_recursion_depth = 0);
+
+bool exists(const path &the_file_name);
+
+bool is_uri(const path &the_file_name);
+
+bool is_directory(const path &the_file_name);
+
+bool create_directory(const path &the_file_name);
+
+std::string join_paths(const path &p1, const path &p2);
+
+std::string path_as_uri(const path &p);
+
+const std::string read_file(const path &theUTF8Filename);
+
+std::vector<uint8_t> read_binary_file(const path &theUTF8Filename);
+
+bool write_file(const path &the_file_name, const std::string &the_data);
+
+bool write_file(const path &the_file_name, const std::vector<uint8_t> &the_data);
+
+bool append_to_file(const std::string &the_file_name, const std::string &the_data);
+
+std::string get_filename_part(const path &the_file_name);
+
+std::string get_directory_part(const path &the_file_name);
+
+fs::path search_file(const path &the_file_name, bool use_entire_path = true);
+
+std::string get_working_directory();
+
+std::string get_extension(const path &thePath);
+
+std::string remove_extension(const path &the_file_name);
+
+FileType get_file_type(const path &file_name);
+
+/************************ Exceptions ************************/
+
+class FileNotFoundException : public std::runtime_error
+{
+private:
+    std::string m_file_name;
+public:
+    explicit FileNotFoundException(const std::string &the_file_name) :
+            std::runtime_error(std::string("File not found: ") + the_file_name),
+            m_file_name(the_file_name) {}
+
+    std::string file_name() const { return m_file_name; }
+};
+
+class OpenDirectoryFailed : public std::runtime_error
+{
+public:
+    explicit OpenDirectoryFailed(const std::string &theDir) :
+            std::runtime_error(std::string("Could not open directory: ") + theDir) {}
+};
+
+class OpenFileFailed : public std::runtime_error
+{
+public:
+    explicit OpenFileFailed(const std::string &the_file_name) :
+            std::runtime_error(std::string("Could not open file: ") + the_file_name) {}
+};
+}
+}
